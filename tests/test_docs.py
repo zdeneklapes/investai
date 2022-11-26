55b3d7e61ac6fe092fb7f9f6899ea60889cd85dd
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-11-27 00:06:05 +0100

    .

---
+++ tests/test_docs.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class TestDocs:
+    def __init__(self):
+        pass
+
+    def test_docs(self):
+        assert True
+
+    def test_docs2(self):
+        assert False
