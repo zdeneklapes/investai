f9649339b58a4942c47dcefefc6aaa146d63806f
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2023-02-20 01:23:25 +0100

    test_dirs.py

--- src/configuration/tests/test_dirs.py
+++ src/configuration/tests/test_dirs.py
@@ -5,6 +5,11 @@ from configuration.dirs import ExperimentDir
 
 
 class TestExperimentDir:
+    def teardown_class(self):
+        """Delete out directory after all tests are done."""
+        experiment_dir = ExperimentDir(root=Path(__file__).parent)
+        experiment_dir.delete_out_dir()
+
     def test_create_dirs(self):
         experiment_dir = ExperimentDir(root=Path(__file__).parent)
         experiment_dir.create_dirs()
@@ -28,6 +33,21 @@ class TestExperimentDir:
         assert experiment_dir.algo.exists()
         assert experiment_dir.tensorboard.exists()
         assert experiment_dir.results.exists()
+
+    @pytest.mark.depends(on=["test_create_dirs", "test_create_specific_dirs", "test_add_attributes_for_models"])
+    @pytest.mark.parametrize("_id, add_attr", [
+        pytest.param("1", False),
+        pytest.param("2", True),
+    ])
+    def test__get_next_algo_folder_id_1(self, _id, add_attr):
+        """Check when self.algo is None, then return 1."""
+        experiment_dir = ExperimentDir(root=Path(__file__).parent)
+
+        if add_attr:
+            experiment_dir.add_attributes_for_models("algo")
+
+        folder_id = experiment_dir._get_next_algo_folder_id()
+        assert folder_id == _id
 
     @pytest.mark.depends(on=["test_create_dirs"])
     def test_delete_out_dir(self):
