b7d042e74c7bee38e7290c4f0bcb2b7de1aaa71e
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2023-03-12 16:03:01 +0100

    program added logger

--- investai/shared/program.py
+++ investai/shared/program.py
@@ -6,6 +6,7 @@ import attr
 
 from shared.projectstructure import ProjectStructure
 from shared.arguments import parse_arguments
+from shared.utils import get_logger
 
 
 @attr.define
@@ -17,3 +18,6 @@ class Program:
     test_date_start: str = attr.field(default='xxx-xx-xx')
     test_date_end: str = attr.field(default='xxx-xx-xx')
     debug = getenv('DEBUG', None)
+
+    def __attrs_post_init__(self):
+        self.logger = get_logger(self.project_structure.out.joinpath('run.log').as_posix())
