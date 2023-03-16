1d7131b59a1a39947f1764d5c2e951da4743a9e3
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2023-03-16 22:16:03 +0100

    add test declaration
    This is a combination of 2 commits.
    This is the 1st commit message:
    
    add test declaration
    
    This is the commit message #2:
    
    add test declaration

---
+++ investai/run/portfolio_allocation/tests/test_run_all_scripts.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_run_baseline(): pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_run_dataset(): pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_run_wandbtrain(): pass
